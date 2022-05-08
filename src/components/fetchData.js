ae364630a05b1f371086ed1ff6948ca1c47967a5
abharathore06 <aabharathore86862@gmail.com>
2022-05-08 21:41:35 +0530

    Added required css files

--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
 import CommentsAdd from './commentsAdd';
+import './fetchData.css';
+
 class FetchData extends Component {
     constructor(props) {
         super(props);
-        
-      }
+    }
 
   render() {
     let currentDate = new Date();
     let weatherIcon = "http://openweathermap.org/img/w/" + this.props.weatherIcon + ".png"
     return (
    <div>
-        <div style={{background: '#FFFFFF', boxShadow: '0px 4px 32px rgba(143, 143, 143, 0.4)', borderRadius: '4px', padding: '10px', marginTop: '10px'   , marginLeft: '15%',  marginRight: '15%'}}>
-      <span style={{ fontSize: '20px', color: '#505050' }}>
-          {this.props.cityName}, {this.props.cityCountry},  {currentDate.toDateString()} 
-      </span>
-    <br/>
-      <div
-      style={{display: 'flex',flexDirection: 'row'}}>
-      <div
-      style={{  width: '60%' , paddingLeft: '5%', paddingTop: '58px'}}>
-             <h1> <span  style={{ color: '#505050', fontSize: '100px' }}> {this.props.cityTemperature}&deg;C               <img src={weatherIcon}/>
-</span> 
-</h1>
-      </div>
-      <div
-      style={{  width: '40%', paddingTop: '30px'}}>
-        <p style={{color: '#505050', marginTop:'-25px', fontSize: '22px'}}>
-         Weather: <b>{this.props.cityWeatherDescription}</b><br/>
-         Wind:  <b>{this.props.cityWind} km/hr</b> 
+      <div className='App-Fetch'>
+        <span className='Header-Details'>
+            {this.props.cityName}, {this.props.cityCountry},  {currentDate.toDateString()} 
+        </span>
         <br/>
-        <br/>
-        Humidity: <b>{this.props.cityHumidity}%</b> <br/>
-        Pressure: <b> {this.props.cityPressure} Pa</b>
-       <br/>
-       <br/>
-         Max Temp: <b>{this.props.cityMaxTmp}&deg;C </b><br/>
-         Min Temp: <b>{this.props.cityMinTmp}&deg;C</b>
-        <br/> <br/>
-        Sunrise : <b> {this.props.citySunrise}</b><br/>
-        Sunset: <b> {this.props.citySunset}</b>
-       <br/>
-        </p>
+        <div className='App-Fetch-b'>
+
+          <div className='App-Fetch-c'>
+            <h1> 
+              <span className='Main-Temp-Detail'>{this.props.cityTemperature}&deg;C               
+              <img src={weatherIcon}/>
+              </span> 
+            </h1>
+          </div>
+
+          <div className='App-Fetch-d'>
+            <p className='Temperature-Details'>
+              Weather: <b>{this.props.cityWeatherDescription}</b><br/>
+              Wind:  <b>{this.props.cityWind} km/hr</b><br/>
+              <br/>
+
+              Humidity: <b>{this.props.cityHumidity}%</b> <br/>
+              Pressure: <b> {this.props.cityPressure} Pa</b><br/>
+              <br/>
+
+              Max Temp: <b>{this.props.cityMaxTmp}&deg;C </b><br/>
+              Min Temp: <b>{this.props.cityMinTmp}&deg;C</b><br/> 
+              <br/>
+
+              Sunrise : <b> {this.props.citySunrise}</b><br/>
+              Sunset: <b> {this.props.citySunset}</b><br/>
+
+            </p>
+
+          </div>
+        </div>
 
       </div>
-      </div>
-      </div>
-      <div
-      style={{background: '#FFFFFF', boxShadow: '0px 4px 32px rgba(143, 143, 143, 0.4)', borderRadius: '4px', padding: '10px', marginTop: '10px' ,  paddingBottom: '10px'  , marginLeft: '15%',  marginRight: '15%'}}
-
-      >
-               <CommentsAdd></CommentsAdd>
       
+      <div className='addCommentsBox'>
+        <CommentsAdd></CommentsAdd>
       </div>
+
       <br/>
       <br/>
    </div>
